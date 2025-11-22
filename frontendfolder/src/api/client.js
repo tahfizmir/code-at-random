fb293fe5fbf5a6c760aefcf6609d0407a6cdae31
tahfizmir <tahfeezmir27@gmail.com>
2025-11-22 16:23:17 +0530

    some changes to make dynamic switching while on local and production

--- frontendfolder/src/api/client.js
+++ frontendfolder/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+const BASE_URL=location.hostname==='localhost'?"http://localhost:5000":"https://code-path-backend.onrender.com";
 const client = axios.create({
-  baseURL: 'https://code-path-backend.onrender.com',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
